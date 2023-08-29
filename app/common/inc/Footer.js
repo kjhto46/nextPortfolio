490534036c9464cfecba171e73fd83fdc70e7920
권준희 <81128476+kjhto46@users.noreply.github.com>
2023-08-29 14:31:59 +0900

    푸터 업로드

--- app/common/inc/Footer.js
+++ app/common/inc/Footer.js
@@ -1,9 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import classes from "../css/Footer.module.scss";
 
 export default function Footer() {
-    return(
-        <footer></footer>
+    return (
+        <footer className={classes.footer}>
+            <div className={classes.inner}>
+                <div className={classes.copyright}>
+                    <p className="poppins">Copyright 2023 kwonjunhee All Right Reserved</p>
+                    <Link href={"/"}>
+                        Go To Top
+                    </Link>
+                </div>
+            </div>
+        </footer>
     )
 }
