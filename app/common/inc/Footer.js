5ea1333562006a5db9bbc1da053982b2fc788ee0
kjhto46 <tony1996kjh@gmail.com>
2023-08-31 00:55:14 +0900

    푸터 애니메이션은 구현완료 스타일은 추가할예정

--- app/common/inc/Footer.js
+++ app/common/inc/Footer.js
@@ -1,42 +1,143 @@
 "use client";
 
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+
 import Link from "next/link";
 import classes from "../css/Footer.module.scss";
+import OrignBtn from "@/app/components/OrignBtn";
+
+function Parallaxcloud({ children, baseVelocity = 100 }) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  const wrap = (min, max, v) => {
+    if (v < min) {
+      return max - ((min - v) % (max - min));
+    }
+    return min + ((v - min) % (max - min));
+  };
+
+  const x = useTransform(baseX, (v) => `${wrap(-66.66, -33.33, v)}%`);
+
+  const directionFactor = useRef(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    /**
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
+     */
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
+  return (
+    <motion.div className={classes.cloudScroll} style={{ x }}>
+      {children}
+      {children}
+      {children}
+    </motion.div>
+  );
+}
 
 export default function Footer() {
-
-    const scrollToTop = () => {
-        window.scroll({
-            top: 0,
-            behavior: 'smooth'
-        })
-
-    }
-    return (
-        <footer className={`poppins ${classes.footer}`}>
-            <div className={classes.inner}>
-                <div className={classes.footerLink}>
-                    <div>
-                        <h2>Text undecided</h2>
-                    </div>
-                    <div></div>
-                </div>
-                <div className={classes.copyright}>
-                    <p>Copyright 2023 kwonjunhee All Right Reserved</p>
-                    <div onClick={scrollToTop}>
-                        <p>
-                        Go To Top
-                        </p>
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="12" viewBox="0 0 10 12" fill="none">
-                                <g clipPath="url(#clip0_1023_2953)">
-                                    <path d="M10 4.76L6.31 1.24L5 0L3.69 1.24L0 4.76L1.31 6L4 3.44V12H6V3.44L8.69 6L10 4.76Z" className={classes.fillColor} />
-                                </g>
-                            </svg>
-                        </span>
-                    </div>
-                </div>
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <footer className={`poppins ${classes.footer}`}>
+      <Parallaxcloud baseVelocity={-0.5}>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </Parallaxcloud>
+      <div>
+        <div className={classes.inner}>
+          <div className={classes.footerLink}>
+            <div className={classes.leftArea}>
+              <h2>Text undecided</h2>
+              <Link href={"/contact"} className="orignBtn orgWBtn">
+              <OrignBtn viewtxt="Contact" />
+              </Link>
             </div>
-        </footer>
-    )
+            <div className={classes.rightArea}>
+              <h4>Contact Me</h4>
+              <ul>
+                <li>
+                  <a href="https://github.com/kjhto46" target="_blank">https://github.com/kjhto46</a>
+                </li>
+                <li>
+                  <a href="mailto:tony1996kjh@naver.com">tony1996kjh@naver.com</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={classes.copyright}>
+            <p>Copyright 2023 kwonjunhee All Right Reserved</p>
+            <div onClick={scrollToTop}>
+              <p>Go To Top</p>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="12"
+                  viewBox="0 0 10 12"
+                  fill="none"
+                >
+                  <g clipPath="url(#clip0_1023_2953)">
+                    <path
+                      d="M10 4.76L6.31 1.24L5 0L3.69 1.24L0 4.76L1.31 6L4 3.44V12H6V3.44L8.69 6L10 4.76Z"
+                      className={classes.fillColor}
+                    />
+                  </g>
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/components/FileScroll.js
+++ app/components/FileScroll.js
@@ -4,8 +4,7 @@ import { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 
 const fileVariants = {
-  start: {
-  },
+  start: {},
   end: {
     transition: {
       type: "linear",
@@ -14,68 +13,83 @@ const fileVariants = {
   },
 };
 
-const circleVariants = [{
-  start: {
-    rotate: 0,
+const circleVariants = [
+  {
+    start: {
+      rotate: 0,
+    },
+    center: {
+      rotate: 0,
+    },
+    end: {
+      x: 10,
+      y: 50,
+      rotate: -4,
+    },
   },
-  center:{
-    rotate:0,
+  {
+    start: {
+      rotate: 0,
+    },
+    center: {
+      rotate: 0,
+    },
+    end: {
+      x: -10,
+      y: 50,
+      rotate: 4,
+    },
   },
-  end: {
-    x: 10,
-    y: 50,
-    rotate: -4,
+  {
+    start: {
+      y: 60,
+      rotate: 0,
+    },
+    center: {
+      y: 60,
+      rotate: 0,
+    },
+    end: {
+      y: 60,
+      rotate: 0,
+    },
   },
-}, {
-  start: {
-    rotate: 0,
-  },
-  center:{
-    rotate:0,
-  },
-  end: {
-    x: -10,
-    y: 50,
-    rotate: 4,
-  },
-}, {
-  start: {
-    y: 60,
-    rotate: 0,
-  },
-  center:{
-    y: 60,
-    rotate:0,
-  },
-  end: {
-    y: 60,
-    rotate:0,
-  },
-}
 ];
 
 const FileScroll = () => {
   const items = [
-    ["크레이티브그룹 아더", "축구공 안녕하세요", ""],
-    ["🏀", "농구공", ""],
-    ["🏈", "럭비공", ""],
+    ["크레이티브그룹 아더", "반응형 페이지", "2023.01", "http://arthur.co.kr/", ""],
+    ["AMPK", "반응형 페이지", "2022.09", "http://ampk.1004home.kr/", ""],
+    ["동수원병원", "반응형 페이지", "2023.01", "https://www.dswhosp.co.kr/", ""]
   ];
 
   return (
     <div className="mainProScroll">
       <div className="sticky">
-        {items.map(([contTit, contxt, contImg], i) => (
-          <motion.div variants={fileVariants} initial="start" whileInView="end" viewport={{ once:false, amount: 0.95 }} className={`file file${i}`} key={i}>
-            <motion.div
-              variants={circleVariants[i]}
-              className="fileCon"
-            >
-              <div className="fileName">
-                <h2>{contTit}</h2>
-              </div>
+        {items.map(([contTit, contxt1, contxt2, contxt3, contImg], i) => (
+          <motion.div
+            variants={fileVariants}
+            initial="start"
+            whileInView="end"
+            viewport={{ once: false, amount: 0.95 }}
+            className={`file file${i}`}
+            key={i}
+          >
+            <motion.div variants={circleVariants[i]} className="fileCon">
+              <div className="fileHead"></div>
+              <div className="flexAreabg"></div>
               <div className="flexArea">
-                <figure><img src={contImg} alt={contTit} /></figure>
-                <div className="txtArea">{contxt}</div>
+                <figure>
+                  <img src={contImg} alt={contTit} />
+                </figure>
+                <div className="txtArea">
+                  <h2>{contTit}</h2>
+                  <ul>
+                    <li>{contxt1}</li>
+                    <li>{contxt2}</li>
+                    <li><a href={contxt3} target="_blank">페이지 이동</a></li>
+                  </ul>
+                </div>
               </div>
             </motion.div>
           </motion.div>
