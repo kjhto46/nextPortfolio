d3710140928225505a133f96ae81eb9c8de131e6
kjhto46 <tony1996kjh@gmail.com>
2023-09-11 14:49:37 +0900

    1차로 디테일 페이지 작업 완료

--- app/portfolio/[portId]/page.js
+++ app/portfolio/[portId]/page.js
@@ -1,48 +1,69 @@
 import Footer from "@/app/common/inc/Footer";
 import Header from "@/app/common/inc/Header";
+import OrignBtn from "@/app/components/OrignBtn";
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import Link from "next/link";
 import { notFound, useRouter } from "next/navigation";
 
 export default async function PortDetail(props) {
-  if(props.params.portId.toString('hex')){}
+  if (props.params.portId.toString("hex")) {
+  }
   const client = await connectDB;
   const db = client.db("portfolio");
   let portResult = await db
     .collection("portfolioDetail")
     .findOne({ _id: new ObjectId(props.params.portId) });
-  console.log(portResult);
 
   if (portResult === null) {
     return notFound();
   } else {
     return (
       <>
-      <Header />
+        <Header />
         <title>{portResult.ptitle}</title>
         <div className="portSub">
           <article className="visual">
-            <Link href={'/portfolio'}>뒤로가기</Link>
+            <div className="inner">
+              <Link href={"/portfolio"} className="poppins">
+                Back to List
+              </Link>
+            </div>
             <h2>{portResult.ptitle}</h2>
           </article>
-          <article>
+          <article className="detailArt">
             <div className="inner">
-              <ul>
-                <li>
-                  <h4>Technologies</h4>
-                  <ol>
-                    <li>테크 1</li>
-                    <li>테크 2</li>
-                  </ol>
-                </li>
-                <li>
-                  <h4>Website URl</h4>
-                  <a href="https://www.naver.com" target="_blank">
-                    https://www.naver.com
-                  </a>
-                </li>
-              </ul>
+              <div className="flexArea">
+                <div className="left">
+                  <h2>{portResult.ptitle}</h2>
+                  <Link
+                    href={portResult.pUrl}
+                    target="_blank"
+                    className="orignBtn orgBtn"
+                  >
+                    <OrignBtn viewtxt="Web Site" />
+                  </Link>
+                </div>
+                <div className="right">
+                  <ul>
+                    <li>
+                      <h3>Date</h3>
+                      <p>{portResult.pDate}</p>
+                    </li>
+                    <li>
+                      <h3>Skill Set</h3>
+                      {portResult?.skillSet?.length > 0 && (
+                        <ol>
+                          {portResult.skillSet.map((skill, skillIndex) => (
+                            <li key={skillIndex}>{skill}</li>
+                          ))}
+                        </ol>
+                      )}
+                    </li>
+                    <li className="subtxt">{portResult.subtxt}</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </article>
         </div>
