2a51fdfb0c0e5aa04b7c31e67a85394663465256
kjhto46 <tony1996kjh@gmail.com>
2023-09-16 19:50:43 +0900

    메인 반응형 확인

--- app/common/inc/Header.js
+++ app/common/inc/Header.js
@@ -70,14 +70,9 @@ export default function Header() {
                 <g clipPath="url(#clip0_727_435)">
                   <path
                     d="M13.3332 13.3338C9.64984 13.3338 6.6665 10.3505 6.6665 6.66712C6.6665 4.72546 7.50817 2.97546 8.83317 1.75879C4.78317 2.32546 1.6665 5.79212 1.6665 10.0005C1.6665 14.6005 5.39984 18.3338 9.99984 18.3338C14.2082 18.3338 17.6748 15.2171 18.2415 11.1671C17.0248 12.4921 15.2748 13.3338 13.3332 13.3338Z"
-                    fill="#ED7B34"
+                    fill="#1E1E1E"
                   />
                 </g>
-                <defs>
-                  <clipPath id="clip0_727_435">
-                    <rect width="20" height="20" fill="white" />
-                  </clipPath>
-                </defs>
               </svg>
               <svg
               className="lightSvg"
@@ -90,70 +85,65 @@ export default function Header() {
                 <g clipPath="url(#clip0_740_656)">
                   <path
                     d="M9.99984 13.3337C11.8408 13.3337 13.3332 11.8413 13.3332 10.0003C13.3332 8.15938 11.8408 6.66699 9.99984 6.66699C8.15889 6.66699 6.6665 8.15938 6.6665 10.0003C6.6665 11.8413 8.15889 13.3337 9.99984 13.3337Z"
-                    fill="#192227"
+                    fill="#1E1E1E"
                   />
                   <path
                     d="M13.0085 2.5L12.3418 4.16667"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M6.8252 2.56641L7.5252 4.22474"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M2.5 6.99121L4.16667 7.65788"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M2.56689 13.1746L4.22523 12.4746"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M6.9917 17.4997L7.65837 15.833"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M13.1751 17.4337L12.4751 15.7754"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M17.5002 13.0085L15.8335 12.3418"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                   <path
                     d="M17.4332 6.8252L15.7749 7.5252"
-                    stroke="#192227"
+                    stroke="#1E1E1E"
                     strokeWidth="2"
                     strokeMiterlimit="10"
                     strokeLinecap="round"
                   />
                 </g>
-                <defs>
-                  <clipPath id="clip0_740_656">
-                    <rect width="20" height="20" fill="white" />
-                  </clipPath>
-                </defs>
               </svg>
           </div>
         </nav>
