bddc94a75a2bd12aea325da069d7266819529b2d
kjhto46 <tony1996kjh@gmail.com>
2023-09-25 00:26:21 +0900

    확인용 업데이트

--- app/contact/Comment.js
+++ app/contact/Comment.js
@@ -22,7 +22,7 @@ export default function ShowComment() {
   useEffect(() => {
     fetchComments();
   }, []);
-  
+
   const [isActive, setIsActive] = useState(false);
 
   const handleFocus = () => {
@@ -34,25 +34,25 @@ export default function ShowComment() {
   };
 
   const handleDelete = (_id) => {
-  	const password = prompt("비밀번호를 입력하세요.");
-    console.log(_id)
-  	
-  	if (!password) {
-  	  return; // 비밀번호가 입력되지 않으면 삭제 요청 중단
-  	}
+    const password = prompt("비밀번호를 입력하세요.");
+    console.log(_id);
 
-  	fetch("/api/comment/delete", {
-  	  method: "POST",
-  	  body: JSON.stringify({
-  	    _id,
-  	    password,
-  	  }),
-  	  headers: {
-  	    "Content-Type": "application/json",
-  	  },
-  	})
-  	  .then((res) => res.json())
-  	  .then((result) => {
+    if (!password) {
+      return; // 비밀번호가 입력되지 않으면 삭제 요청 중단
+    }
+
+    fetch("/api/comment/delete", {
+      method: "POST",
+      body: JSON.stringify({
+        _id,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
         alert(result.message);
         fetchComments(); // 댓글 목록 갱신
       })
@@ -69,9 +69,13 @@ export default function ShowComment() {
                 <h3>{a.author}</h3>
                 <span>{a.date}</span>
                 <p>{a.content}</p>
-                <button onClick={() => {
-                handleDelete(a._id)
-                }}>X</button>
+                <button
+                  onClick={() => {
+                    handleDelete(a._id);
+                  }}
+                >
+                  X
+                </button>
               </li>
             ))
           ) : (
@@ -83,84 +87,89 @@ export default function ShowComment() {
           )}
         </ul>
       </div>
-      <div className={`inputArea ${isActive ? 'active' : ''}`}>
+      <div className={`inputArea ${isActive ? "active" : ""}`}>
         <div className="bg"></div>
         <div className="inputDiv">
-          <input
-            onChange={(e) => {
-              setCommName(e.target.value);
-            }}
-            value={commName}
-            ref={inputE1}
-            placeholder="닉네임을 입력해주세요."
-          />
-          <input
-            onChange={(e) => {
-              setCommPass(e.target.value);
-            }}
-            type="password"
-            value={commPass}
-            ref={inputE2}
-            placeholder="비밀번호를 입력해주세요."
-          />
-          <textarea
-            onChange={(e) => {
-              setComment(e.target.value);
-            }}
-            value={comment}
-            ref={textareaE1}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            placeholder="내용을 입력해주세요."
-          />
+          <ul>
+            <li>
+              <input
+                onChange={(e) => {
+                  setCommName(e.target.value);
+                }}
+                value={commName}
+                ref={inputE1}
+                placeholder="닉네임을 입력해주세요."
+              />
+              <input
+                onChange={(e) => {
+                  setCommPass(e.target.value);
+                }}
+                type="password"
+                value={commPass}
+                ref={inputE2}
+                placeholder="비밀번호를 입력해주세요."
+              />
+            </li>
+            <li>
+              <textarea
+                onChange={(e) => {
+                  setComment(e.target.value);
+                }}
+                value={comment}
+                ref={textareaE1}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                placeholder="내용을 입력해주세요."
+              />
+            </li>
+          </ul>
         </div>
-
       </div>
       <div className="commentBtn">
         <button
-            onClick={() => {
-              if (commName === "") {
-                alert("이름을 입력해주세요.");
-                return inputE1.current.focus();
-              }
-              if (comment === "") {
-                alert("비밀번호를 입력해주세요");
-                return inputE2.current.focus();
-              }
-              if (comment === "") {
-                alert("내용을 입력해주세요");
-                return textareaE1.current.focus();
-              }
-              const now = new Date();
-              let year = now.getFullYear();
-              let month = now.getMonth();
-              let date = now.getDate();
-              let hours = now.getHours();
-              let minutes = now.getMinutes();
-              let seconds = now.getSeconds();
-              const krDate = `${year}년 ${month}월 ${date}일 ${hours}:${minutes}:${seconds}`;
-              console.log(commName, comment);
-              fetch("/api/comment/new", {
-                method: "POST",
-                body: JSON.stringify({
-                  author: commName,
-                  comment: comment,
-                  password: commPass,
-                  date: krDate,
-                }),
-              })
-                .then((r) => r.json())
-                .then((result) => {
-                  fetchComments();
-                  setCommName("");
-                  setCommPass("");
-                  setComment("");
-                });
-            }}
-          >
-            댓글 전송
-          </button>
-          </div>
+          onClick={() => {
+            if (commName === "") {
+              alert("이름을 입력해주세요.");
+              return inputE1.current.focus();
+            }
+            if (comment === "") {
+              alert("비밀번호를 입력해주세요");
+              return inputE2.current.focus();
+            }
+            if (comment === "") {
+              alert("내용을 입력해주세요");
+              return textareaE1.current.focus();
+            }
+            const now = new Date();
+            let year = now.getFullYear();
+            let month = now.getMonth();
+            let date = now.getDate();
+            let hours = now.getHours();
+            let minutes = now.getMinutes();
+            let seconds = now.getSeconds();
+            const krDate = `${year}년 ${month}월 ${date}일 ${hours}:${minutes}:${seconds}`;
+            console.log(commName, comment);
+            fetch("/api/comment/new", {
+              method: "POST",
+              body: JSON.stringify({
+                author: commName,
+                comment: comment,
+                password: commPass,
+                date: krDate,
+              }),
+            })
+              .then((r) => r.json())
+              .then((result) => {
+                fetchComments();
+                setCommName("");
+                setCommPass("");
+                setComment("");
+              });
+          }}
+        >
+          댓글 전송
+        </button>
+      </div>
     </>
   );
 }
