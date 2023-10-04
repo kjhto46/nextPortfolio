9f1813cb503d276d509c2124c08c62b23d2e99d0
kjhto46 <tony1996kjh@gmail.com>
2023-10-04 10:40:22 +0900

    loading 디자인 2

--- app/portfolio/[portId]/loading.js
+++ app/portfolio/[portId]/loading.js
@@ -1,9 +1,17 @@
 import "./../../common/css/sub.scss";
+import "./../../globals.scss";
 
 export default function loading() {
    return(
          <div className="loadingDiv">
-            <h4>로딩중</h4>
+         <div className="portSub">
+           <article
+             className="visual"
+             style={{ background: `rgba(227,229,245,1)`}}
+           >
+
+           </article>
+         </div>
          </div>
       )
 }
