000ea41bfe489f572941eee0c0e9fe4082432946
kjhto46 <tony1996kjh@gmail.com>
2023-10-04 12:20:18 +0900

    날짜 오류

--- app/contact/Comment.js
+++ app/contact/Comment.js
@@ -144,7 +144,7 @@ export default function ShowComment() {
             }
             const now = new Date();
             let year = now.getFullYear();
-            let month = now.getMonth();
+            let month = (now.getMonth()+1);
             let date = now.getDate();
             let hours = now.getHours();
             let minutes = now.getMinutes();
