e2a9ae6e72d9ffb5267a774315b45c9483ff389d
kjhto46 <tony1996kjh@gmail.com>
2023-10-04 11:27:35 +0900

    loading skeleton style 작업 진행 중 3

--- app/portfolio/[portId]/loading.js
+++ app/portfolio/[portId]/loading.js
@@ -3,15 +3,38 @@ import "./../../globals.scss";
 
 export default function loading() {
    return(
-         <div className="loadingDiv">
-         <div className="portSub">
+         <div className="portSub loading">
            <article
              className="visual"
              style={{ background: `rgba(227,229,245,1)`}}
            >
-
            </article>
-         </div>
+
+          <article className="detailArt">
+            <div className="inner">
+                <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+
+              <div className="viewTit">
+                <h2>&nbsp;</h2>
+              </div>
+              <div className="viewEtc">
+               <ul>
+                  <li>
+                     <h3>&nbsp;</h3>
+                     <p>&nbsp;</p>
+                  </li>
+                  <li>
+                     <h3>&nbsp;</h3>
+                     <p>&nbsp;</p>
+                  </li>
+                  <li>
+                     <h3>&nbsp;</h3>
+                     <p>&nbsp;</p>
+                  </li>
+               </ul>
+               </div>
+            </div>
+          </article>
          </div>
       )
 }
--- app/portfolio/loading.js
+++
@@ -1,9 +0,0 @@
-import "./../common/css/sub.scss";
-
-export default function loading() {
-   return(
-         <div className="loadingDiv">
-            <h4>로딩중123</h4>
-         </div>
-      )
-}
